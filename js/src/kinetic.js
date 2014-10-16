2c9663c45d9814bc04f9dce796dac5fe356ed52d
Ethel Ng <ethel.ng@gmail.com>
2014-10-16 03:42:00 +0100

    More housekeeping

--- js/src/kinetic.js
+++ js/src/kinetic.js
@@ -2,22 +2,14 @@ $(document).ready(function() {
 
   var stage = new Kinetic.Stage({
     container: 'container',
-    width: 14400,
-    height: 7200
+    width: 1440,
+    height: 720
   });
 
   var layer = new Kinetic.Layer();
 
-  var scale = 1;
-  var min_scale = 0.1;
-
-  $('#graph').bind('mousewheel MozMousePixelScroll',    function(event, delta, deltaX, deltaY){
-    event.preventDefault();
-    onMouseWheel(event,delta,deltaX,deltaY);
-  });
-
   var xhr = new XMLHttpRequest();
-  xhr.open('GET', 'data/coords.txt', false);
+  xhr.open('GET', 'data/world.txt', false);
   xhr.send(null);
   var data = xhr.responseText.split('\n');
   var dataArray = JSON.parse("[" + data + "]");
@@ -25,8 +17,8 @@ $(document).ready(function() {
   dataArray.forEach(function(point){
     layer.add(
       new Kinetic.Rect({
-        x: (point[1]+180)*5,
-        y: (180-(point[0]+90))*5,
+        x: (point[1]+180)*4,
+        y: (180-(point[0]+90))*4,
         width: 2,
         height: 2,
         fill: 'black',
@@ -59,61 +51,6 @@ $(document).ready(function() {
 //   stage.add(layer);
 // })
 
-stage.add(layer);
+  stage.add(layer);
 
 });
-
-function onMouseWheel(e, delta,dx,dy) {
-  
-  // mozilla fix...
-  if (e.originalEvent.detail){
-    delta = e.originalEvent.detail;
-  }
-  else{
-    delta = e.originalEvent.wheelDelta;
-  }
-  
-  if (delta !== 0) {
-    e.preventDefault();
-  }
-
-  var cur_scale;
-  if (delta > 0) {
-    cur_scale = scale + Math.abs(delta / 640);
-  } else {
-    cur_scale = scale - Math.abs(delta / 640);
-  }
-
-  console.log('>>>>',e,delta,e.detail);
-  //check for minimum scale limit
-  console.log(cur_scale, min_scale);
-  if (cur_scale > min_scale) {
-    
-    var d=document.getElementById('graph');
-    var cnvsPos=getPos(d);
-    var Apos = stage.getAbsolutePosition();
-    var mousePos = stage.getPosition();
-    
-    console.log(d,cnvsPos,Apos,mousePos);
-    var smallCalc  = (e.originalEvent.pageY - Apos.x - cnvsPos.x)/scale;
-    var smallCalcY = (e.originalEvent.pageY - Apos.y - cnvsPos.y)/scale;
-
-    var endCalc = (e.originalEvent.pageY - cnvsPos.x) - cur_scale*smallCalc;
-    var endCalcY = (e.originalEvent.pageY - cnvsPos.y) - cur_scale*smallCalcY;
-
-    scale = cur_scale;
-
-    stage.setPosition( endCalc, endCalcY);
-
-     layer.setScale(cur_scale);
-     layer.draw();
-  }
-
-}
-
-function getPos(el){
-  for (var lx=0, ly=0;
-       el != null;
-       lx += el.offsetLeft, ly += el.offsetTop, el = el.offsetParent);
-    return {x: lx,y: ly};
-}
